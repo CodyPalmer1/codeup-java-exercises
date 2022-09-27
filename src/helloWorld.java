c6b14b89279707279eff88d29f076373098c7f9b
Cody Palmer <codyryanpalmer@gmail.com>
2022-09-27 15:09:02 -0500

    helloWorld.java exercises complete

---
+++ src/helloWorld.java
@@ -0,0 +1,48 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "My String!";
+        System.out.println(myString);
+
+        myString = "New string";
+        myString = "3.14159";
+
+        float myNumber;
+        //myNumber = 3.14;
+        myNumber = 123L;
+        System.out.println(myNumber);
+
+        // int x = 5;
+        // System.out.println(x++);
+        // System.out.println(x);
+
+        //int x = 5;
+        //System.out.println(++x);
+        //System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x = x + 5;
+//        x=+5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+//        byte num = 127;
+//        System.out.println(num++);
+//        System.out.println(++num);
+    }
+}
