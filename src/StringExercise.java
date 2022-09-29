a97fff17a1991b2d8c367afd80fa5a61a8ee9816
Cody Palmer <codyryanpalmer@gmail.com>
2022-09-29 14:39:16 -0500

    Updates to both StringExercise and Bob

---
+++ src/StringExercise.java
@@ -0,0 +1,13 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String words = "We don't need no education \n We don't need no thought control";
+        System.out.println(words);
+        String words2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(words2);
+        String words3 = "In windows, the main drive is usually C:\\";
+        System.out.println(words3);
+        String words4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(words4);
+    }
+}
