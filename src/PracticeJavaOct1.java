e55b4dbbf5eab72f13375f1116476ce4089808aa
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-02 09:00:19 -0500

    PracticeJavaOct1 updated and improved.

--- src/PracticeJavaOct1.java
+++ src/PracticeJavaOct1.java
@@ -4,13 +4,17 @@ import java.lang.Math;
 public class PracticeJavaOct1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Are you trying to find the circumfrence of a circle?");
+        System.out.println("Are you trying to find the information of a circle?");
         String userInput = scanner.nextLine();
         if(userInput.equalsIgnoreCase("Yes")){
             System.out.println("Please enter a number for the radius: ");
             int radius = scanner.nextInt();
-            double circumfrence = (2*(Math.PI * radius));
-            System.out.printf("The circufrence of circle with a radius of %s is: " + circumfrence, radius);
+            double diameter = (2 * radius);
+            System.out.printf("The diameter of circle with a radius of %s is: " + diameter, radius);
+            double circumference = (2*(Math.PI * radius));
+            System.out.printf("\nThe circumference of circle with a radius of %s is: " + circumference, radius);
+            double area = (Math.PI * (radius * radius));
+            System.out.printf("\nThe area of circle with a radius of %s is: " + circumference, radius);
         } else {
             System.out.println("Sorry this is not the right program for you.");
         }
