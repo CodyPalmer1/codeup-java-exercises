4fa79a169593c654fda004b6d6b0b48ee5c3bf35
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-03 11:01:06 -0500

    Updates to MethodLecture:

--- src/MethodLecture.java
+++ src/MethodLecture.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class MethodLecture {
     public static void main(String[] args) {
        int num1;
@@ -15,10 +14,22 @@ public class MethodLecture {
         System.out.println(getPassword());
         System.out.println(isLoggedIn());
         System.out.println(multiply(5.3, 5.7));
+        System.out.println(add(num1, num2));
+        System.out.println(add(5));
+
+        recursion();
     }
 
     public static int add(int num1, int num2){
         return num1 + num2;
+    }
+
+    public static int add(double num1, double num2){
+        return (int)(num1 + num2);
+    }
+
+    public static int add(double num1){
+        return (int)(num1 + num1);
     }
 
     public static String getPassword(){
@@ -40,5 +51,19 @@ public class MethodLecture {
     public static double multiply(double d1, double d2){
         double answer = (d1*d2);
         return answer;
+    }
+
+    public static void recursion(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Do you want to continue?[y/n] ");
+        String choice = scanner.nextLine();
+        if(choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("yes")){
+            recursion();
+        } else if (choice.equalsIgnoreCase("n") || choice.equalsIgnoreCase("no")){
+            System.exit(0);
+        } else {
+            System.out.println("Invalid Input.");
+            recursion();
+        }
     }
 }
