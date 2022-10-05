ef6bca43edc4fb8fc5146a2021ac8039b80b60d6
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-05 11:05:44 -0500

    Updates to input and inputTest exercises

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Person {
     private String name;
 
@@ -30,6 +29,25 @@ public class Person {
         String newName = scanner.nextLine();
         person.setName(newName);
         System.out.println(person.getName());
+
+//        ------------------------------------------------
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
     }
 
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,42 +1,43 @@
 package util;
 import java.util.Scanner;
 public class Input {
-    private final Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
-    public String getString(){
+    public static String getString(){
         return scanner.nextLine();
     }
 
-    public boolean yesNo(){
+    public static boolean yesNo(){
         String userInput = scanner.nextLine();
-        return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("Yes");
+        return (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("Yes") || userInput.equalsIgnoreCase("true"));
     }
 
-    public int getInt(int min, int max){
-        System.out.println("Please enter a number between 1 - 10: ");
+    public static int getInt(int min, int max){
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.printf("Please enter a number between %s - %s: ", min, max);
         int userInt = scanner.nextInt();
         if(userInt < min || userInt > max){
-            System.out.println("Invalid number. Please enter a number in range: ");
-            getInt();
+            System.out.println("Invalid number. Please enter a number in range: ");;
+            userInt = getInt(min, max);
         }
         return userInt;
     }
 
-    public int getInt(){
+    public static int getInt(){
         return scanner.nextInt();
     }
 
-    public double getDouble(double min, double max){
-        System.out.println("Please enter a number between 1 - 10: ");
-        double userDouble = scanner.nextInt();
+    public static double getDouble(double min, double max){
+        System.out.printf("Please enter a number between %s - %s: ", min, max);
+        double userDouble = scanner.nextDouble();
         if(userDouble < min || userDouble > max){
             System.out.println("Invalid number. Please enter a number in range: ");
-            getInt();
+            userDouble = getDouble(min, max);
         }
         return userDouble;
     }
 
-    public double getDouble(){
+    public static double getDouble(){
         return scanner.nextDouble();
     }
 }
