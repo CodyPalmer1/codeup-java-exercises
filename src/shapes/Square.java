e56097a0e0cf39a05547b8d1629bae7f728761ef
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-07 08:32:15 -0500

    Updates to shapes exercises

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,17 +1,23 @@
 package shapes;
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
 
-    protected int side;
-
-    public Square(int side){
-        super(side, side);
+    @Override
+    public double getPerimeter() {
+        return 0;
     }
 
-    public double getArea(){
-        return 4 * side;
+    @Override
+    public double getArea() {
+        return 0;
     }
 
-    public double getPerimeter(){
-        return Math.pow(side, 2);
+    @Override
+    public void setLength(double length) {
+
+    }
+
+    @Override
+    public void setWidth(double width) {
+
     }
 }
