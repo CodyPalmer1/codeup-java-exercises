dd189cff0550f9d18482f29aea06855a3e201c9f
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-06 20:00:00 -0600

    Updates to shapes exercises

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,18 +1,23 @@
 package shapes;
-public class Rectangle {
-    protected int width;
-    protected int length;
+public class Rectangle extends Quadrilateral implements Measurable{
 
-    public Rectangle(int width, int length){
-        this.width = width;
-        this.length = length;
+    @Override
+    public double getPerimeter() {
+        return 0;
     }
 
-    public double getArea(){
-        return length * width;
+    @Override
+    public double getArea() {
+        return 0;
     }
 
-    public double getPerimeter(){
-        return (2 * length) + (2 * width);
+    @Override
+    public void setLength(double length) {
+
+    }
+
+    @Override
+    public void setWidth(double width) {
+
     }
 }
--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,13 +1,22 @@
 package shapes;
-
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
-        Rectangle box2 = new Square(5);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+        Measurable myShape;
+        Square myShape = new Square();
+        Rectangle myShape = new Rectangle();
 
-        System.out.println(box2.getPerimeter());
-        System.out.println(box2.getArea());
+//        Rectangle box1 = new Rectangle(4, 5);
+//        System.out.println(box1.getPerimeter());
+//        System.out.println(box1.getArea());
+
+        System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getArea());
+
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getPerimeter());
+//        System.out.println(box2.getArea());
+
+        System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getArea());
     }
 }
