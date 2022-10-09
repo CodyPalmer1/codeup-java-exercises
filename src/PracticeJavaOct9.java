5634bbc96a8fc0ee7ece042ff7d7ad5a0928232f
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-09 16:54:24 -0500

    Practice exercise updates

---
+++ src/PracticeJavaOct9.java
@@ -0,0 +1,13 @@
+abstract class PracticeJavaOct9 {
+    double length;
+    double width;
+    double height;
+
+    abstract void getLength();
+
+    abstract void getWidth();
+
+    abstract void getHeight();
+
+    abstract void setVolume();
+}
---
+++ src/PractiveJavaOct9Extends.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PractiveJavaOct9Extends extends PracticeJavaOct9 {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        getLength();
+        getWidth();
+        getHeight();
+        setVolume();
+    }
+    void getLength() {
+        System.out.println("Please enter a length: ");
+        double userLength = scanner.nextInt();
+    }
+
+    void getWidth() {
+        System.out.println("Please enter a width: ");
+        double userWidth = scanner.nextInt();
+    }
+
+    void getHeight() {
+        System.out.println("Please enter a height: ");
+        double userHeight = scanner.nextInt();
+    }
+
+    void setVolume() {
+        userVolume =
+    }
+}
